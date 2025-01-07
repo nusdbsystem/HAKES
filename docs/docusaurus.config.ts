5d1f671149a021edceda40457a797765ce6fead5
tinyAdapter <tinyadapter@outlook.com>
2025-01-07 03:27:32 +0000

    docs: fix build target url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://www.comp.nus.edu.sg',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/~dbsystem/hakes/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
