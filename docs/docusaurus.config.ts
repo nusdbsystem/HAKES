3f714c7b6831164054b4759d75045ffb2cef4a3d
tinyAdapter <tinyadapter@outlook.com>
2025-01-07 03:07:56 +0000

    docs: fix footer & copyright

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -88,8 +88,8 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Docs',
+              to: '/',
             },
           ],
         },
@@ -124,7 +124,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} NUS Data System Research Group. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
